919ed063e0591d7eb97fdd9b665d79ca50072375
Дарья <109847625+daryatalantseva@users.noreply.github.com>
2022-08-07 10:15:34 +0300

    Update Program.cs

--- pr2/Program.cs
+++ pr2/Program.cs
@@ -6,15 +6,15 @@ number2 = Convert.ToInt32(Console.ReadLine());
 number3 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2 & number1 > number3 )
     {
-        Console.WriteLine("Первое число больше и оно равно" + number1);
+        Console.WriteLine("Первое число больше и оно равно " + number1);
     }
 if (number2 > number1 & number2 > number3 ) 
     {
-        Console.Write("Второе число больше и оно равно" + number2);
+        Console.Write("Второе число больше и оно равно " + number2);
     }
 if (number3 > number2 & number3 > number1 ) 
     {
-        Console.WriteLine("Третье число больше и оно равно" + number3);
+        Console.WriteLine("Третье число больше и оно равно " + number3);
     }
     
         
